bb69f067075397c1a4b1898c229c616fe753e966
Emil Madsen <sovende@gmail.com>
2020-02-07 22:04:10 +0100

    Debug print entire span

--- ext/opentelemetry-ext-asgi/src/opentelemetry/ext/asgi/__init__.py
+++ ext/opentelemetry-ext-asgi/src/opentelemetry/ext/asgi/__init__.py
@@ -80,10 +80,10 @@ class OpenTelemetryMiddleware:
         span_name = get_default_span_name(scope)
 
         with self.tracer.start_as_current_span(span_name, parent_span, kind=trace.SpanKind.SERVER, attributes=collect_request_attributes(scope)) as connection_span:
-            print("connection", connection_span.get_context())
+            print("connection", connection_span)
             async def wrapped_receive():
                 with self.tracer.start_as_current_span(span_name + " (unknown-receive)") as receive_span:
-                    print("wrapped_receive", receive_span.get_context())
+                    print("wrapped_receive", receive_span)
                     payload = await receive()
                     if payload['type'] == "websocket.receive":
                         receive_span.set_attribute("http.status_code", 200)
@@ -95,7 +95,7 @@ class OpenTelemetryMiddleware:
 
             async def wrapped_send(payload):
                 with self.tracer.start_as_current_span(span_name + " (unknown-send)") as send_span:
-                    print("wrapped_send", send_span.get_context())
+                    print("wrapped_send", send_span)
                     if payload['type'] == "http.response.start":
                         status_code = payload['status']
                         try:
